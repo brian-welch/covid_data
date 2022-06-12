059ecbdc6049a7477167695d710478343f694083
Brian Welch <myself@brianwelch.se>
2022-06-12 09:34:43 +0200

    syntax error corrected

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
 
 function myLoad($class) {
-    
     foreach(['queries', 'componants', 'elements', 'forms', 'base', 'functions'] as $prefix) {
         if(file_exists("{$_SERVER['DOCUMENT_ROOT']}/covid_data/lib/$prefix/$class.php")) {
             include_once("{$_SERVER['DOCUMENT_ROOT']}/covid_data/lib/$prefix/$class.php");
         }
     }
 }
+spl_autoload_register('myLoad');
 
 GlobalVariables::errorBundle();
 
-spl_autoload_register('myLoad');
 
 include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/header_blocks/header_index.php";
 
