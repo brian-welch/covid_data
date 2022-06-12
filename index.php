c0d8aed99779df53a347cabbd3f2cf3f9e75526a
Brian Welch <myself@brianwelch.se>
2022-06-12 09:31:15 +0200

    root dierctory reference added to ~/index.php

--- index.php
+++ index.php
@@ -11,11 +11,11 @@ function myLoad($class) {
 
 spl_autoload_register('myLoad');
 
-include 'page_blocks/header_blocks/header_index.php';
+include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/header_blocks/header_index.php";
 
-include 'page_blocks/body_blocks/body_index.php';
+include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/body_blocks/body_index.php";
 
-include 'page_blocks/footer_blocks/footer_index.php';
+include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/footer_blocks/footer_index.php";
 
 /*
 $_SERVER['DOCUMENT_ROOT'] needed??
