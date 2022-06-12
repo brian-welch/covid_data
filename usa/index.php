3a59b3967aab2efa5ede3fc1131c2ca69822c0e6
Brian Welch <myself@brianwelch.se>
2022-06-12 08:46:57 +0200

    new directory and index file

---
+++ usa/index.php
@@ -0,0 +1,22 @@
+<?php
+
+function myLoad($class) {
+    
+    foreach(['queries', 'componants', 'elements', 'forms', 'base', 'functions'] as $prefix) {
+        if(file_exists("{$_SERVER['DOCUMENT_ROOT']}/covid_data/lib/$prefix/$class.php")) {
+            include_once("{$_SERVER['DOCUMENT_ROOT']}/covid_data/lib/$prefix/$class.php");
+        }
+    }
+}
+
+spl_autoload_register('myLoad');
+
+include 'page_blocks/header_blocks/header_index.php';
+
+include 'page_blocks/body_blocks/body_index.php';
+
+include 'page_blocks/footer_blocks/footer_index.php';
+
+/*
+$_SERVER['DOCUMENT_ROOT'] needed??
+ */
