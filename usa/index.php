31ae5c793ca36d68f86965ea810039fc4d1b5fdc
Brian Welch <myself@brianwelch.se>
2022-06-12 09:28:03 +0200

    root directory references in usa/index.php plus some line cleanup

--- usa/index.php
+++ usa/index.php
@@ -1,24 +1,19 @@
 <?php
 
-GlobalVariables::errorBundle();
-
 function myLoad($class) {
-    
     foreach(['queries', 'componants', 'elements', 'forms', 'base', 'functions'] as $prefix) {
         if(file_exists("{$_SERVER['DOCUMENT_ROOT']}/covid_data/lib/$prefix/$class.php")) {
             include_once("{$_SERVER['DOCUMENT_ROOT']}/covid_data/lib/$prefix/$class.php");
         }
     }
 }
-
 spl_autoload_register('myLoad');
 
-include 'page_blocks/header_blocks/header_index.php';
+GlobalVariables::errorBundle();
 
-include 'page_blocks/body_blocks/body_index.php';
 
-include 'page_blocks/footer_blocks/footer_index.php';
+include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/header_blocks/header_index.php";
 
-/*
-$_SERVER['DOCUMENT_ROOT'] needed??
- */
+include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/body_blocks/body_index.php";
+
+include "{$_SERVER['DOCUMENT_ROOT']}/covid_data/page_blocks/footer_blocks/footer_index.php";
